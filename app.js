d1947b662c94b078a8cbf554fab35908d0e21901
SJ.Lee <eagleduk@gmail.com>
2022-11-02 22:18:53 +0900

    [27 Days] 236. 객체 속성 액세스

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let age = 22;
 let name = "Nam";
 let hobbies = ["Sports", "Cooking", "Reading"];
-let job = { title: "Developer", place: "New York", salary: 30000 };
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 30000,
+};
 
 alert(hobbies[0]);
+alert(job.title);
