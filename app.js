be4585ed3ae4cb08cf0139e5d74406f8365cd8ab
SJ.Lee <eagleduk@gmail.com>
2022-11-15 21:56:57 +0900

    [30 Days] 278. 첫번째 '클릭' 이벤트 리스너 추가하기

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-// document.body.children[1].children[0].href = "https://google.com";
+let paragraphElement = document.querySelector("p");
 
-let anchorElement = document.getElementById("external-link");
-anchorElement.href = "https://google.com";
+function changeParagraphText() {
+  paragraphElement.textContent = "clicked!";
+  console.log("Paragraph Click");
+}
 
-anchorElement = document.querySelector("p a");
-anchorElement.href = "https://udemy.com";
-
-let newAnchorElement = document.createElement("a");
-newAnchorElement.href = "https://google.com";
-newAnchorElement.textContent = "This is lead to google";
-
-// 273. 자바 스크립트를 통해 새 HTML 요소 삽입
-
-let firstParagragraph = document.querySelector("p");
-
-firstParagragraph.append(newAnchorElement);
-
-// 274. DOM 요소 삭제
-
-let firstH1Element = document.querySelector("h1");
-
-// 현재 방식
-firstH1Element.remove();
-// 구 방식
-// firstH1Element.parentElement.removeChild(firstH1Element);
-
-// 275. 기존 요소 이동
-firstParagragraph.parentElement.append(firstParagragraph);
-
-// 276. 'innerHTML' 작업
-firstParagragraph.innerHTML = "This is <strong>important!</strong>";
+paragraphElement.addEventListener("click", changeParagraphText);
