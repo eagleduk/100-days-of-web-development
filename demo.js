d67765afb47d0e04f275e692b7ecba53f7317a0d
SJ.Lee <eagleduk@gmail.com>
2022-11-16 21:53:01 +0900

    [31 Days] 294. 'else' & 'else if'가 있는 대안

--- demo.js
+++ demo.js
@@ -6,9 +6,14 @@ let maxLength = inputElement.maxLength;
 function inputLength(event) {
   const remainingLength = maxLength - event.target.value.length;
   inputText.innerHTML = remainingLength;
-  if (remainingLength < 10) {
+  if (remainingLength === 0) {
+    inputText.classList.add("error");
+    event.target.classList.add("error");
+  } else if (remainingLength <= 10) {
     inputText.classList.add("warning");
     event.target.classList.add("warning");
+    inputText.classList.remove("error");
+    event.target.classList.remove("error");
   } else {
     inputText.classList.remove("warning");
     event.target.classList.remove("warning");
