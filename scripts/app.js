95a8dc0acbd7a71d5ec39301e51daa80a31808b1
SJ.Lee <eagleduk@gmail.com>
2022-11-29 23:58:34 +0900

    [self] Tic Tac Toe

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 let selectedPlayerIndex = 0;
+let playingGame = false;
 
 const players = [
   { name: "", symbol: "X" },
   { name: "", symbol: "O" },
 ];
+
+let game = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+let startPlayer = 0;
 
 const formElement = document.querySelector("form");
 
@@ -20,3 +29,19 @@ player2EditButton.addEventListener("click", openModalPopup);
 cancelPlayerName.addEventListener("click", closeModalPopup);
 
 formElement.addEventListener("submit", confirmPlayerName);
+
+const startButton = document.getElementById("game-start-button");
+
+startButton.addEventListener("click", gameStart);
+
+const gameBoard = document.getElementById("game-board");
+
+const gameActiveBoard = document.getElementById("game-active-board");
+gameActiveBoard.querySelectorAll("li").forEach((element) => {
+  element.addEventListener("click", selectedBoard);
+});
+
+const playerTurn = document.getElementById("player-turn");
+
+const gameOverBoard = document.getElementById("game-over");
+const gameWinner = document.getElementById("game-winner");
---
+++ scripts/game.js
@@ -0,0 +1,93 @@
+function gameStart() {
+  if (!(players[0].name && players[1].name)) {
+    alert("enter player names");
+    return;
+  }
+
+  playingGame = true;
+
+  gameOverBoard.style.display = "none";
+  gameBoard.style.display = "block";
+
+  for (let i = 0; i < gameActiveBoard.children.length; i++) {
+    const row = Math.floor(i / 3);
+    const col = i % 3;
+
+    game[row][col] = i;
+    gameActiveBoard.children[i].textContent = "";
+    gameActiveBoard.children[i].classList.remove("selected");
+  }
+
+  const nowPlayer = players[startPlayer];
+  playerTurn.textContent = nowPlayer.name;
+}
+
+function gameOver(name) {
+  gameOverBoard.style.display = "block";
+  gameWinner.textContent = name;
+}
+
+function checkGameOver() {
+  // '/' 로직 검사
+  if (
+    game[2][0] &&
+    game[1][1] &&
+    game[0][2] &&
+    game[2][0] === game[1][1] &&
+    game[1][1] === game[0][2]
+  )
+    return true;
+
+  // '\' 로직 검사
+  if (
+    game[0][0] &&
+    game[1][1] &&
+    game[2][2] &&
+    game[0][0] === game[1][1] &&
+    game[1][1] === game[2][2]
+  )
+    return true;
+
+  // '|' 로직 검사
+  if (
+    (game[0][0] === game[1][0] && game[1][0] === game[2][0]) ||
+    (game[0][1] === game[1][1] && game[1][1] === game[2][1]) ||
+    (game[0][2] === game[1][2] && game[1][2] === game[2][2])
+  )
+    return true;
+
+  // '-' 로직 검사
+  if (
+    (game[0][0] === game[0][1] && game[0][1] === game[0][2]) ||
+    (game[1][0] === game[1][1] && game[1][1] === game[2][1]) ||
+    (game[2][0] === game[2][1] && game[2][1] === game[2][2])
+  )
+    return true;
+
+  return false;
+}
+
+function selectedBoard(event) {
+  const nowPlayer = players[startPlayer];
+
+  if (!playingGame || event.target.classList.contains("selected")) return;
+
+  const boardIndex = event.target.dataset.boardIndex;
+  event.target.classList.add("selected");
+  event.target.textContent = nowPlayer.symbol;
+
+  const row = Math.floor(+boardIndex / 3);
+  const col = +boardIndex % 3;
+
+  game[row][col] = nowPlayer.symbol;
+
+  const result = checkGameOver(row, col);
+
+  if (result) {
+    gameOver(nowPlayer.name);
+    playingGame = false;
+  } else {
+    startPlayer = startPlayer === 0 ? 1 : 0;
+    playerTurn.textContent = players[startPlayer].name;
+  }
+}
