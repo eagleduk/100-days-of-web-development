c085c6a992e87632d23971d6336fe05354d48b00
SJ.Lee <eagleduk@gmail.com>
2022-12-07 21:59:34 +0900

    [37 Days] 362. 노드 & 익스프레스로 HTML 파일 제공

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!!</h1>");
+  const htmlFile = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFile);
+});
+app.get("/confirm", function (req, res) {
+  const htmlFile = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFile);
+});
+app.get("/about", function (req, res) {
+  const htmlFile = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFile);
+});
+app.get("/recommend", function (req, res) {
+  const htmlFile = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFile);
+});
+app.get("/restaurants", function (req, res) {
+  const htmlFile = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFile);
 });
 
 app.listen(3000);
