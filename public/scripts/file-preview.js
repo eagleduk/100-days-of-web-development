d4d8961b59e09167c23d0ab2837c5a4667f22e99
SJ.Lee <eagleduk@gmail.com>
2022-12-26 22:41:29 +0900

    [41 Days] 476. 이미지 미리보기 기능 추가

--- public/scripts/file-preview.js
+++ public/scripts/file-preview.js
@@ -2,6 +2,10 @@ const previewImgElement = document.getElementById("preview");
 
 document.getElementById("image").addEventListener("change", function (e) {
   const [file] = e.target.files;
-  const imageSrc = URL.createObjectURL(file);
-  preview.src = imageSrc;
+  if (file) {
+    const imageSrc = URL.createObjectURL(file);
+    preview.src = imageSrc;
+  } else {
+    preview.src = "";
+  }
 });
