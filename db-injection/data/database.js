53f762383aeaf54a4e0699fedc0c7910ddbd6fd0
SJ.Lee <eagleduk@gmail.com>
2023-01-04 22:28:52 +0900

    [44 Days] 516. SQL 주입 공격으로부터 보호

--- db-injection/data/database.js
+++ db-injection/data/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: "your-pw",
+  multipleStatements: true, // 한번에 여러 SQL 명령을 수행
+});
 
 module.exports = pool;
