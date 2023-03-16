3f9e220378541f7384c8f3b338a1551b8c1cabbe
SJ.Lee <eagleduk@gmail.com>
2023-03-16 21:29:32 +0900

    Vue JS 에 API 연결
    Vue JS 에 todo API 연결 스스로 해보기

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -11,31 +11,117 @@ Vue.createApp({
     };
   },
   /**
+   *      Vue JS 가 마운트 되고 나서 실행
+   * @returns
+   */
+  async mounted() {
+    let response;
+    try {
+      response = await fetch("http://localhost:3000/todos");
+    } catch (error) {
+      alert("Something went wrong!");
+      return;
+    }
+
+    if (!response.ok) {
+      alert("Something went wrong!");
+      return;
+    }
+
+    const responseData = await response.json();
+    this.todos = responseData.todos;
+  },
+  /**
    *    DOM 이벤트에 사용할 Event 작성
    */
   methods: {
-    submit(event) {
+    async submit(event) {
       event.preventDefault();
       if (this.id) {
         const id = this.id;
         const text = this.text;
+
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos/" + this.id, {
+            method: "PATCH",
+            body: JSON.stringify({
+              text: this.text,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
         this.todos = this.todos.map((todo) =>
-          todo.id === id ? { id, text } : todo
+          todo._id === id ? { _id: id, text } : todo
         );
       } else {
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.text,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+        const todoId = responseData.todo.id;
+
         this.todos.push({
           text: this.text,
-          id: Date.now(),
+          _id: todoId,
         });
       }
       this.text = "";
     },
     editItem(id) {
       this.id = id;
-      this.text = this.todos.find((todo) => todo.id === id).text;
+      this.text = this.todos.find((todo) => todo._id === id).text;
     },
-    deleteItem(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
+    async deleteItem(id) {
+      console.log(id);
+      let response;
+
+      try {
+        response = await fetch("http://localhost:3000/todos/" + id, {
+          method: "DELETE",
+        });
+      } catch (error) {
+        alert("Something went wrong!");
+        return;
+      }
+
+      if (!response.ok) {
+        alert("Something went wrong!");
+        return;
+      }
+
+      this.todos = this.todos.filter((todo) => todo._id !== id);
     },
   },
 }).mount("#app");
